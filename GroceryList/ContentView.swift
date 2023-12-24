76bc5fe37d998416b546840fac20333965638af9
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-12-24 18:23:34 -0500

    Add NavigationView with textfield and button

--- GroceryList/ContentView.swift
+++ GroceryList/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var context
+    @State var newItemString = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                TextField("Add item", text: $newItemString)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button("Save") {
+                    guard !newItemString.isEmpty else {
+                        return
+                    }
+                    
+                    let newItem = GroceryListItem(title: newItemString,
+                                                  subtitle: "Buy this ASAP!",
+                                                  date: Date())
+                    
+                    context.insert(newItem)
+                }
+                
+                List {
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
